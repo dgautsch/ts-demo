08756196219b77b0cf386a3838865bc5f343a4b6
Dan Gautsch <dgautsch@users.noreply.github.com>
2020-01-15 21:08:18 -0500

    initial commit

---
+++ demo.ts
@@ -0,0 +1,22 @@
+// # Difference between JavaScript and Typescript
+
+// ### Boolean
+let isValid:boolean;
+
+// ### Number
+
+// ### String
+
+// ### Object
+
+// ### Array
+
+// ### Any
+
+// ### Void
+
+// ### Null and Undefined
+
+// ## Types and Intefaces
+
+// # Typescript Features
