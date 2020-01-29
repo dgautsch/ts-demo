b4b11b0cd80acc04c5832ea06fad50d0f254a101
Dan Gautsch <dgautsch@users.noreply.github.com>
2020-01-29 12:46:34 -0500

    add demo files

---
+++ examples.ts
@@ -0,0 +1,117 @@
+// # Difference between JavaScript and Typescript
+
+// ## Primitive or Basic Types
+
+// ### Boolean
+let isValid: boolean;
+
+isValid = true;
+
+console.log(isValid);
+
+// ### Number
+let myNumber:number;
+
+myNumber = 2;
+
+// ### String
+let myString:string;
+
+myString = 'hello';
+
+// ### Object
+let myObject:object;
+
+myObject = {}
+
+// ### Array
+let myArray:Array<string> | string[]
+myArray = ['one', 'two']
+
+let myObjectArray:object[]
+myObjectArray = [{hello: 'world'}]
+
+// ### Any
+let anyType:any;
+anyType = 'hello';
+anyType = []
+anyType = {}
+
+// ## Types and Intefaces
+let cart: CartItem[] = [];
+
+// Use types if you need to use a union
+type CartItem = {
+    sku: string;
+    name: string;
+    quantity: number;
+}
+
+// use interfaces if you need to extend
+interface Cart {
+    info: object[],
+    type: string,
+    id: string,
+    buckets: object[]
+}
+
+function addCartItem(item: CartItem): CartItem[] {
+    cart.push(item);
+    return cart;
+}
+
+const results = addCartItem({
+    name: 'Pants',
+    quantity: 1,
+    sku: 'SKU1'
+});
+
+console.log(results)
+
+// # Typescript Features
+
+// ## Type inference
+// https://www.typescriptlang.org/docs/handbook/type-inference.html#contextual-typing
+let x = 3;
+
+// x = '2'
+
+// ## Contextual Typing
+// https://www.typescriptlang.org/docs/handbook/type-inference.html#contextual-typing
+
+// ## Extend Interface
+interface Item {
+    sku?: string;
+    name?: string;
+    quantity?: number;
+    getName(s: string): string;
+}
+interface BopisItem extends Item {
+    stsStock?: number;
+    storeStock?: number;
+}
+
+const ispuItem: BopisItem = {
+    getName() {
+        return 'hello';
+    }
+}
+
+// ## Type Aliases
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-aliases
+
+type Name = string;
+type NameResolver = () => string;
+type NameOrResolver = Name | NameResolver;
+function getName(n: NameOrResolver): Name {
+    if (typeof n === "string") {
+        return n;
+    }
+    else {
+        return n();
+    }
+}
+
+console.log(getName(() => {
+    return 'Dan';
+}))
---
+++ index.ts
@@ -0,0 +1,61 @@
+// Write a TypeScript application that accomplishes the following:
+// - Write an interface for the products collection called Products.
+// - Write an interface that extends the product interface and adds the property
+//   `fullyReserved` as a boolean. Call this interface ReservedProducts.
+// - Caclulate if the item is fullyReserved by checking if the reserved is less than
+//   the directStock.
+// - If the reserved is greater than the directStock set fullyReserved to `true`.
+// - Return an new collection of type ReservedProducts
+// - Invoke your function and output the results via console.log
+
+const products = [{
+    color: 'red',
+    reserved: 10,
+    directStock: 20,
+},
+{
+    color: 'blue',
+    reserved: 12,
+    directStock: 10,
+},{
+    color: 'green',
+    reserved: 7,
+    directStock: 30,
+},{
+    color: 'orange',
+    reserved: 45,
+    directStock: 10,
+},{
+    color: 'blue',
+    reserved: 1,
+    directStock: 13,
+},{
+    color: 'blue',
+    reserved: 12,
+    directStock: 5,
+},{
+    color: 'yellow',
+    reserved: 10,
+    directStock: 99,
+}]
+
+interface Product {
+    color: string;
+    reserved: number;
+    directStock: number;
+}
+
+interface ReservedProduct extends Product {
+    fullyReserved: boolean;
+}
+
+function getFullyReserved(products: Product[]): ReservedProduct[] {
+    return products.map<ReservedProduct>((product: Product) => {
+        return {
+            ...product,
+            fullyReserved: product.reserved > product.directStock
+        }
+    })
+}
+
+console.log(getFullyReserved(products));
